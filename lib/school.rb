759ef9d4e66ed33b4b41c7222229d98085683071
generous-vector-9581 <ojappah@gmail.com>
2019-10-08 01:32:57 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,13 @@
 # code here!
+class School
+  
+  attr_reader : roster
+  
+  def initialize (name)
+    @roster = {}
+  end
+  
+  def add_student()
+    @ROSTER = {grade =>{[student]}
+  end
+end
